a00d5ee76d2c9e4154fec880443e050d384c798a
KUNALD1 <kunaldhak1@gmail.com>
2014-04-29 22:10:25 +0530

    s

--- readfile.pl
+++ readfile.pl
@@ -4,34 +4,31 @@ use warnings;
 my %count;
 my $directory = "D:\\EN-US";
 
-opendir(DIR, $directory)
+opendir( DIR, $directory )
   || die "Unable to open directory - $!\n";
+
 #my @files = grep /\.txt/, readdir( DIR );
-my @files =readdir( DIR );
-    
-
-
-#open( OUTFILE, ">> $directory\\COID_LIST.txt" )
-#  || die "Unable to open write file! - $!\n";  
+my @files = readdir(DIR);
 
 foreach my $file (@files) {
-	next if($file =~ /^\.$/);
-    next if($file =~ /^\.\.$/);
-	my $path=$directory."\\".$file;
-#	print "$path \n";
-    open(FILE,"<", $path) or die $!;
-    if (-e $path ) {
-# print "%FILE \n";
-print "$path ::\n";
- while (my $row = <FILE>) {
- 	chomp($row);
- 	print "$row \n";
- }
-}else{
-   print "$file doesn't exists\n";
-}
+	next if ( $file =~ /^\.$/ );
+	next if ( $file =~ /^\.\.$/ );
+	my $path = $directory . "\\" . $file;
 
-    close(FILE);
+	#	print "$path \n";
+	open( FILE, "<", $path ) or die $!;
+	if ( -e $path ) {
 
+		# print "%FILE \n";
+		print "$path ::\n";-
+		while ( my $row = <FILE> ) {
+			chomp($row);
+			print "$row \n";
+		}
+	}
+	else {
+		print "$file doesn't exists\n";
+	}
+	close(FILE);
 }
 closedir(DIR);
